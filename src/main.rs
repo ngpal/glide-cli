55e1e45db1a30475867d79994b5389313cc40613
Nandagopal Menon <nandagopalnmenon@gmail.com>
2024-12-25 17:58:29 +0530

    Client updated to handle commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use regex::Regex;
 use std::io::Write;
-use std::{fs, io};
-use tokio::fs::File;
+use std::io::{self, BufRead};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 
@@ -9,65 +9,80 @@ const CHUNK_SIZE: usize = 1024;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
-    println!("Connected to server");
+    println!("Connected to server!");
+
+    let mut username = String::new();
 
     loop {
-        // Get filename
-        let mut file_name = String::new();
-
-        print!("Enter the file name to send (or type 'exit' to quit): ");
+        username.clear();
+        print!("Enter your username: ");
         io::stdout().flush()?;
+        io::stdin().read_line(&mut username)?;
 
-        io::stdin().read_line(&mut file_name)?;
+        let username = username.trim();
 
-        let file_path = file_name.trim();
-        if file_path.to_lowercase() == "exit" {
-            println!("Exiting...");
+        if validate_username(&username) {
             break;
         }
 
-        let file_length;
+        println!(
+            "Invalid username!
+Usernames must follow these rules:
+    • Only alphanumeric characters and periods (.) are allowed.
+    • Must be 1 to 10 characters long.
+    • Cannot start or end with a period (.).
+    • Cannot contain consecutive periods (..).
 
-        // Send file metadata
-        match fs::metadata(&file_path) {
-            Ok(metadata) => {
-                file_length = metadata.len();
-                stream
-                    .write_all(format!("{}:{}", &file_path, file_length).as_bytes())
-                    .await?;
-                println!("File metadata sent to the server");
-            }
-            Err(e) => {
-                println!("Error locating the file: {}", e);
-                continue;
-            }
+Please try again with a valid username."
+        )
+    }
+
+    stream
+        .write_all(format!("username {}\n", username).as_bytes())
+        .await?;
+
+    // Command loop
+    let stdin = io::stdin();
+    let mut input = String::new();
+
+    println!("Type 'help' to see available commands.");
+
+    loop {
+        // Get user input
+        input.clear();
+        print!("glide> ");
+        io::stdout().flush()?;
+        stdin.lock().read_line(&mut input)?;
+
+        let command = input.trim();
+
+        if command == "exit" {
+            println!("Thank you for using Glide. Goodbye!");
+            break;
         }
 
-        // Calculate the number of chunks
-        let partial_chunk_size = file_length % CHUNK_SIZE as u64;
-        let chunk_count = file_length / CHUNK_SIZE as u64 + (partial_chunk_size > 0) as u64;
+        // Send command to the server
+        stream
+            .write_all(format!("{}\n", command).as_bytes())
+            .await?;
 
-        // Read and send chunks
-        let mut file = File::open(file_path).await?;
-        let mut buffer = vec![0; CHUNK_SIZE];
-
-        for count in 0..chunk_count {
-            let bytes_read = file.read(&mut buffer).await?;
-            if bytes_read == 0 {
-                break;
-            }
-
-            stream.write_all(&buffer[..bytes_read]).await?;
-            println!(
-                "Sent chunk {}/{} ({}%)",
-                count + 1,
-                chunk_count,
-                ((count + 1) as f64 / chunk_count as f64 * 100.0) as u8
-            );
+        // Await server response
+        let mut response = vec![0; CHUNK_SIZE];
+        let bytes_read = stream.read(&mut response).await?;
+        if bytes_read == 0 {
+            println!("Server disconnected.");
+            break;
         }
 
-        println!("File transfer completed successfully!");
+        // Print server response
+        let response_str = String::from_utf8_lossy(&response[..bytes_read]);
+        println!("{}", response_str);
     }
 
     Ok(())
+}
+
+fn validate_username(username: &str) -> bool {
+    let re = Regex::new(r"^[a-zA-Z0-9](?:[a-zA-Z0-9\.]{0,8}[a-zA-Z0-9])?$").unwrap();
+    re.is_match(username)
 }
