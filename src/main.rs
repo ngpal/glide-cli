061e9bcd272f49f76a538b621e9477fe777b5f6d
Nandagopal Menon <nandagopalnmenon@gmail.com>
2025-01-23 13:30:50 +0530

    Added support for backspace

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,15 @@
+mod repl;
 use crossterm::{
     self,
-    event::{read, Event, KeyCode},
-    execute,
-    style::Print,
     terminal::{disable_raw_mode, enable_raw_mode},
 };
-use std::io::{self, stdout};
+use repl::Repl;
+use std::io;
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
-
-    loop {
-        match read()? {
-            Event::Key(event) => match event.code {
-                KeyCode::Char('q') => {
-                    break;
-                }
-                KeyCode::Char(ch) => execute!(stdout(), Print(ch))?,
-                _ => continue,
-            },
-            _ => continue,
-        };
-    }
+    let mut repl = Repl::default();
+    repl.run()?;
 
     disable_raw_mode()?;
     println!();
