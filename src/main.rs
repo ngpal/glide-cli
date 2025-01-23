83276f323ab5313b4f41dd11568a59535a1c4ee9
Nandagopal Menon <nandagopalnmenon@gmail.com>
2025-01-23 21:09:10 +0530

    Added arrow key support + more
    Added support for moving the cursor around in the buffer with arrow keys, support to process commands with enter key, and proper exiting with disabling raw mode as soon as `Repl` is dropped.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 mod repl;
-use crossterm::{
-    self,
-    terminal::{disable_raw_mode, enable_raw_mode},
-};
+
 use repl::Repl;
 use std::io;
 
 fn main() -> io::Result<()> {
-    enable_raw_mode()?;
-    let mut repl = Repl::default();
+    let mut repl = Repl::new()?;
     repl.run()?;
 
-    disable_raw_mode()?;
-    println!();
     Ok(())
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,24 +1,53 @@
-#![allow(unused)]
-
 use crossterm::{
-    cursor::MoveToColumn,
-    event::{read, Event, KeyCode, KeyEvent},
-    execute,
-    style::Print,
-    terminal::{Clear, ClearType},
-    ExecutableCommand,
+    cursor::{position, MoveToColumn, MoveToNextLine, MoveUp},
+    event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers},
+    execute, queue,
+    style::{Print, PrintStyledContent, Stylize},
+    terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType},
 };
-use std::io::{self, stdout, Stdout};
+use std::{
+    io::{self, stdout, Write},
+    time::Duration,
+};
 
 #[derive(Default)]
+#[allow(unused)]
 pub struct Repl {
     users: Vec<String>,
     buffer: String,
+    cursor_pos: u16,
+}
+
+impl Drop for Repl {
+    fn drop(&mut self) {
+        disable_raw_mode().unwrap();
+        println!("\nBye.");
+    }
 }
 
 impl Repl {
+    pub fn new() -> io::Result<Self> {
+        enable_raw_mode()?;
+        return Ok(Self::default());
+    }
+
+    fn increment_cursor_pos(&mut self, n: u16) {
+        if (self.cursor_pos as usize) < self.buffer.len() {
+            self.cursor_pos = self.cursor_pos.saturating_add(n);
+        }
+    }
+
+    fn decrement_cursor_pos(&mut self, n: u16) {
+        self.cursor_pos = self.cursor_pos.saturating_sub(n);
+    }
+
     pub fn run(&mut self) -> io::Result<()> {
+        execute!(stdout(), PrintStyledContent("> ".bold().blue()))?;
         loop {
+            if !poll(Duration::from_millis(10))? {
+                continue;
+            }
+
             match read()? {
                 Event::Key(event) => match self.handle_key_event(event)? {
                     true => break,
@@ -26,6 +55,8 @@ impl Repl {
                 },
                 _ => continue,
             };
+
+            stdout().flush()?;
         }
 
         Ok(())
@@ -33,29 +64,80 @@ impl Repl {
 
     pub fn handle_key_event(&mut self, event: KeyEvent) -> io::Result<bool> {
         // Returns true if the code should exit
+        if let KeyEvent {
+            code: KeyCode::Char('c'),
+            modifiers: KeyModifiers::CONTROL,
+            ..
+        } = event
+        {
+            return Ok(true);
+        }
+
         match event.code {
-            KeyCode::Char('q') => return Ok(true),
             KeyCode::Char(ch) => {
-                self.buffer.push(ch);
-                self.update_text()?;
+                self.buffer.insert(self.cursor_pos.into(), ch);
+                self.increment_cursor_pos(1);
             }
             KeyCode::Backspace => {
-                self.buffer.pop();
-                stdout().execute(Clear(ClearType::CurrentLine))?;
-                self.update_text()?;
+                if !self.buffer.is_empty() {
+                    self.buffer.remove(self.cursor_pos as usize - 1);
+                    self.decrement_cursor_pos(1);
+                }
             }
+
+            // Process the contents of the buffer and clear when enter is hit
+            KeyCode::Enter => {
+                // Check if we're on the last line, extend by two
+                if size()?.1 - 1 == position()?.1 {
+                    queue!(stdout(), Print("\n\n"), MoveUp(2))?;
+                }
+
+                match self.process_buffer() {
+                    Ok(output) => queue!(
+                        stdout(),
+                        MoveToNextLine(1),
+                        Print(output),
+                        MoveToNextLine(1)
+                    )?,
+                    Err(err) => queue!(
+                        stdout(),
+                        MoveToNextLine(1),
+                        PrintStyledContent("ERROR".bold().red()),
+                        Print(format!(": {}", err)),
+                        MoveToNextLine(1),
+                    )?,
+                };
+
+                self.buffer.clear();
+                self.cursor_pos = 0;
+            }
+
+            // Handle arrow keys
+            KeyCode::Left => self.decrement_cursor_pos(1),
+            KeyCode::Right => self.increment_cursor_pos(1),
             _ => return Ok(false),
         }
 
+        self.update_text()?;
         Ok(false)
     }
 
-    pub fn update_text(&mut self) -> io::Result<()> {
-        execute!(stdout(), MoveToColumn(0), Print(&self.buffer))?;
+    fn update_text(&mut self) -> io::Result<()> {
+        queue!(
+            stdout(),
+            Clear(ClearType::CurrentLine),
+            MoveToColumn(0),
+            PrintStyledContent("> ".bold().blue()),
+            Print(&self.buffer),
+            MoveToColumn(2 + self.cursor_pos)
+        )?;
         Ok(())
     }
 
-    pub fn process_command(cmd: String) -> Result<String, String> {
-        Ok(cmd)
+    fn process_buffer(&self) -> Result<String, String> {
+        match &*self.buffer {
+            "error" => Err("This is a big bad error!".into()),
+            _ => Ok(self.buffer.clone()),
+        }
     }
 }
