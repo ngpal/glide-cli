39ae2009db324f04ede29b2e38f3033eee9fbdc2
Nandagopal Menon <nandagopalnmenon@gmail.com>
2025-01-24 18:26:16 +0530

    Feature: command history
    Added support for up and down keys to traverse command history

--- src/repl.rs
+++ src/repl.rs
@@ -3,7 +3,7 @@ use crossterm::{
     event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers},
     execute, queue,
     style::{Print, PrintStyledContent, Stylize},
-    terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType},
+    terminal::{self, disable_raw_mode, enable_raw_mode, Clear, ClearType},
 };
 use std::{
     io::{self, stdout, Write},
@@ -11,10 +11,9 @@ use std::{
 };
 
 #[derive(Default)]
-#[allow(unused)]
 pub struct Repl {
-    users: Vec<String>,
-    buffer: String,
+    buffer_history: Vec<String>,
+    buffer_idx: usize,
     cursor_pos: u16,
     cur_input_row: u16,
     quit: bool,
@@ -30,16 +29,22 @@ impl Drop for Repl {
 impl Repl {
     pub fn new() -> io::Result<Self> {
         enable_raw_mode()?;
-        Ok(Self::default())
+        Ok(Self {
+            buffer_history: vec![String::new()],
+            buffer_idx: 0,
+            cursor_pos: 0,
+            cur_input_row: 0,
+            quit: false,
+        })
     }
 
-    fn increment_cursor_pos(&mut self, n: u16) {
-        if (self.cursor_pos as usize) < self.buffer.len() {
+    fn inc_cursor_pos(&mut self, n: u16) {
+        if (self.cursor_pos as usize) < self.get_buffer().len() {
             self.cursor_pos = self.cursor_pos.saturating_add(n);
         }
     }
 
-    fn decrement_cursor_pos(&mut self, n: u16) {
+    fn dec_cursor_pos(&mut self, n: u16) {
         self.cursor_pos = self.cursor_pos.saturating_sub(n);
     }
 
@@ -49,7 +54,28 @@ impl Repl {
     }
 
     fn get_output_rows(&self, output: &str) -> io::Result<u16> {
-        Ok(output.len() as u16 / size()?.1 + u16::from(output.len() as u16 % size()?.1 != 0))
+        Ok(output.len() as u16 / terminal::size()?.1
+            + u16::from(output.len() as u16 % terminal::size()?.1 != 0))
+    }
+
+    fn get_buffer(&self) -> &String {
+        self.buffer_history.get(self.buffer_idx).unwrap()
+    }
+
+    fn get_mut_buffer(&mut self) -> &mut String {
+        self.buffer_history.get_mut(self.buffer_idx).unwrap()
+    }
+
+    fn inc_buffer_idx(&mut self) {
+        if self.buffer_idx < self.buffer_history.len() - 1 {
+            self.buffer_idx += 1;
+            self.cursor_pos = self.get_buffer().len() as u16;
+        }
+    }
+
+    fn dec_buffer_idx(&mut self) {
+        self.buffer_idx = self.buffer_idx.saturating_sub(1);
+        self.cursor_pos = self.get_buffer().len() as u16;
     }
 
     pub fn run(&mut self) -> io::Result<()> {
@@ -89,18 +115,34 @@ impl Repl {
 
             // Input
             (KeyCode::Char(ch), _) => {
-                if size()?.1 - 1 == position()?.1 {
+                // If current buffer is not 0, clone it into 0
+                // and set buffer_idx to 0
+                if self.buffer_idx != 0 {
+                    self.buffer_history[0] = self.get_buffer().clone();
+                    self.buffer_idx = 0;
+                }
+
+                if terminal::size()?.1 - 1 == position()?.1 {
                     queue!(stdout(), Print("\n"), MoveUp(1))?;
                     self.cur_input_row -= 1;
                 }
 
-                self.buffer.insert(self.cursor_pos.into(), ch);
-                self.increment_cursor_pos(1);
+                let pos = self.cursor_pos.into();
+                self.get_mut_buffer().insert(pos, ch);
+                self.inc_cursor_pos(1);
             }
             (KeyCode::Backspace, _) => {
-                if !self.buffer.is_empty() && self.cursor_pos != 0 {
-                    self.buffer.remove(self.cursor_pos as usize - 1);
-                    self.decrement_cursor_pos(1);
+                if !self.get_buffer().is_empty() && self.cursor_pos != 0 {
+                    // If current buffer is not 0, clone it into 0
+                    // and set buffer_idx to 0
+                    if self.buffer_idx != 0 {
+                        self.buffer_history[0] = self.get_buffer().clone();
+                        self.buffer_idx = 0;
+                    }
+
+                    let pos = self.cursor_pos as usize - 1;
+                    self.get_mut_buffer().remove(pos);
+                    self.dec_cursor_pos(1);
                 }
             }
 
@@ -109,7 +151,7 @@ impl Repl {
                 let output = self.process_buffer();
 
                 // Check if we're on the last line, extend by two
-                if size()?.1 - 2 >= position()?.1 {
+                if terminal::size()?.1 - 2 >= position()?.1 {
                     // This is horrible code, please forgive me until I figure something out
                     let clear_height =
                         self.get_output_rows(&output.clone().unwrap_or_else(|x| x))? + 1;
@@ -121,35 +163,39 @@ impl Repl {
                     )?;
                 }
 
-                if self.buffer.trim().is_empty() {
+                if self.get_buffer().trim().is_empty() {
                     queue!(stdout(), MoveToNextLine(1))?;
-                    return Ok(());
+                } else {
+                    match output {
+                        Ok(output) => queue!(
+                            stdout(),
+                            MoveToNextLine(1),
+                            Print(output),
+                            MoveToNextLine(1)
+                        )?,
+                        Err(err) => queue!(
+                            stdout(),
+                            MoveToNextLine(1),
+                            PrintStyledContent("ERROR".bold().red()),
+                            Print(format!(": {}", err)),
+                            MoveToNextLine(1),
+                        )?,
+                    };
                 }
 
-                match output {
-                    Ok(output) => queue!(
-                        stdout(),
-                        MoveToNextLine(1),
-                        Print(output),
-                        MoveToNextLine(1)
-                    )?,
-                    Err(err) => queue!(
-                        stdout(),
-                        MoveToNextLine(1),
-                        PrintStyledContent("ERROR".bold().red()),
-                        Print(format!(": {}", err)),
-                        MoveToNextLine(1),
-                    )?,
-                };
-
-                self.buffer.clear();
+                self.buffer_history.insert(0, String::new());
                 self.cursor_pos = 0;
+                self.buffer_idx = 0;
                 self.set_cur_input_row()?;
             }
 
+            // Traverse history
+            (KeyCode::Up, _) => self.inc_buffer_idx(),
+            (KeyCode::Down, _) => self.dec_buffer_idx(),
+
             // Handle arrow keys
-            (KeyCode::Left, _) => self.decrement_cursor_pos(1),
-            (KeyCode::Right, _) => self.increment_cursor_pos(1),
+            (KeyCode::Left, _) => self.dec_cursor_pos(1),
+            (KeyCode::Right, _) => self.inc_cursor_pos(1),
             _ => {}
         }
 
@@ -158,7 +204,7 @@ impl Repl {
     }
 
     fn update_text(&mut self) -> io::Result<()> {
-        let (cols, _) = size()?;
+        let (cols, _) = terminal::size()?;
         queue!(
             stdout(),
             MoveToRow(self.cur_input_row),
@@ -166,7 +212,7 @@ impl Repl {
             Clear(ClearType::FromCursorDown),
             MoveToColumn(0),
             PrintStyledContent("> ".bold().blue()),
-            Print(&self.buffer),
+            Print(&self.get_buffer()),
             MoveToColumn((2 + self.cursor_pos) % cols),
             MoveToRow(self.cur_input_row + (2 + self.cursor_pos) / cols),
         )?;
@@ -174,9 +220,10 @@ impl Repl {
     }
 
     fn process_buffer(&self) -> Result<String, String> {
-        match self.buffer.trim() {
+        match self.get_buffer().clone().trim() {
+            "/history" => Ok(self.buffer_history.join("\n")),
             "error" => Err("This is a big bad error!".into()),
-            _ => Ok(self.buffer.clone()),
+            _ => Ok(self.get_buffer().clone()),
         }
     }
 }
